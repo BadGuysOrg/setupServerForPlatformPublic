15a371be2d2b7dc1858fba46979b59ee09006e5b
Nazar1251s <nazar1251s@gmail.com>
2023-01-04 16:47:13 +0200

    fix

--- install.sh
+++ install.sh
@@ -16,6 +16,45 @@
 
 
 
+
+sleep 3
+spawn sudo apt update
+expect {
+    "Do you want to continue?" {send -- "yes\r"}
+}
+
+sleep 3
+spawn curl -sL https://deb.nodesource.com/setup_16.x | bash
+expect {
+    "Do you want to continue?" {send -- "yes\r"}
+}
+
+sleep 3
+spawn sudo apt -y install nodejs
+expect {
+    "Do you want to continue?" {send -- "yes\r"}
+}
+
+sleep 3
+spawn sudo npm install --global yarn
+expect {
+    "Do you want to continue?" {send -- "yes\r"}
+}
+
+sleep 3
+spawn sudo npm install -g pm2
+expect {
+    "Do you want to continue?" {send -- "yes\r"}
+}
+
+
+
+
+
+
+
+
+
 sleep 5
 spawn sudo apt-get install certbot
 expect {
