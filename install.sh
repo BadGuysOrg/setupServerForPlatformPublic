17cd775966e1b80d7c2e8259b5d675684f35ebd9
Nazar1251s <nazar1251s@gmail.com>
2023-01-05 10:28:13 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
+
+echo "Port $1" >> /etc/ssh/sshd_config
+sudo service sshd restart
+
 expect -c '
   set timeout -1
+
 
   sleep 3
   spawn sudo apt update
@@ -34,62 +40,32 @@ expect -c '
   }
 
 
+  sleep 5
+  spawn sudo apt-get install certbot
+  expect {
+      "Do you want to continue?" {send -- "yes\r"}
+  }
+
+  sleep 5
+  spawn sudo apt install git-all
+  expect {
+      "Do you want to continue?" {send -- "yes\r"}
+  }
+
+  sleep 5
+  spawn sudo apt install mc
+  expect {
+      "Do you want to continue?" {send -- "yes\r"}
+  }
+
+  sleep 5
+  spawn sudo apt install nginx
+  expect {
+      "Do you want to continue?" {send -- "yes\r"}
+  }
+
+
   expect eof
 '
-done
-
-
-
-# spawn sudo npm install -g n
-# spawn sudo n stable
-# spawn n 16.4.0
-# spawn hash -r
-# spawn sudo apt install -y mongodb
-# spawn npm i -g yarn
-# spawn npm i -g pm2
-
-
-
-
-
-
-
-
-
-
-#
-#server_port=2222
-#
-#sudo apt update
-#
-## nodejs, mongo, yarn, pm2
-#sudo apt install nodejs
-#sudo apt install npm
-#sudo npm install -g n
-#sudo n stable
-#n 16.4.0
-#hash -r
-#sudo apt install -y mongodb
-#npm i -g yarn
-#npm i -g pm2
-#
-## certbot
-##sudo add-apt-repository ppa:certbot/certbot
-##sudo apt-get update
-#
-#sudo apt-get install certbot
-#
-## git
-#sudo apt install git-all
-#
-## mc
-#sudo apt install mc
-#
-## nginx
-#sudo apt install nginx
-#
-## ssh
-#echo "Port $server_port" >> /etc/ssh/sshd_config
-#sudo service sshd restart
 
 
