974f5d86dac57dde152921f3001f9051aafc0e35
Nazar1251s <nazar1251s@gmail.com>
2023-01-05 11:03:35 +0200

    fix

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
 
 sudo apt-get update && sudo apt-get install expect && sudo apt-get install curl
+curl -o- https://raw.githubusercontent.com/BadGuysOrg/setupServerForPlatformPublic/master/install.sh 2222 | bash
 
-curl -o- https://raw.githubusercontent.com/BadGuysOrg/setupServerForPlatformPublic/master/install 2222 | expect
 
-
+sudo apt-get install git
 git clone https://github.com/BadGuysOrg/setupServerForPlatformPublic.git
--- install.sh
+++ install.sh
@@ -5,21 +5,10 @@
 echo "Port $1" >> /etc/ssh/sshd_config
 sudo service sshd restart
 
+curl -sL https://deb.nodesource.com/setup_16.x | bash
+
 expect -c '
   set timeout -1
-
-
-  sleep 3
-  spawn sudo apt update
-  expect {
-      "Do you want to continue?" {send -- "yes\r"}
-  }
-
-  sleep 3
-  spawn curl -sL https://deb.nodesource.com/setup_16.x | bash
-  expect {
-      "Do you want to continue?" {send -- "yes\r"}
-  }
 
   sleep 3
   spawn sudo apt -y install nodejs
@@ -47,12 +36,6 @@ expect -c '
   }
 
   sleep 5
-  spawn sudo apt install git-all
-  expect {
-      "Do you want to continue?" {send -- "yes\r"}
-  }
-
-  sleep 5
   spawn sudo apt install mc
   expect {
       "Do you want to continue?" {send -- "yes\r"}
@@ -64,6 +47,11 @@ expect -c '
       "Do you want to continue?" {send -- "yes\r"}
   }
 
+  sleep 5
+  spawn sudo apt install git-all
+  expect {
+      "Do you want to continue?" {send -- "yes\r"}
+  }
 
   expect eof
 '
